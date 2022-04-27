0607a5d4e1e8f3ee2742693c37763c3982d80b2f
marquisss <32654396+marquisss@users.noreply.github.com>
2022-04-27 09:22:30 -0700

    added button and code

---
+++ Assets/Scripts/DrawCards.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCards : MonoBehaviour
+{
+    public GameObject Card1;
+    public GameObject Card2;
+    public GameObject PlayerArea;
+    public GameObject EnemyArea;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+/*
+    public void OnClick() {
+
+    }
+    */
+}
